01596b2bf671e54396c2f4e1933d093fc9e79628
DK <dasha.krutsko@gmail.com>
2023-01-03 14:03:47 +0300

    update

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -36,8 +36,7 @@ public class LoginServlet extends HttpServlet {
         String login = request.getParameter("login");
         String password = request.getParameter("password");
         List<String> userList = users.getUsers();
-
-        if ((userList.contains(login)) && (password.length()>0)  && ( password!=null )) {
+        if ((userList.contains(login)) && (password!=null) && (password.trim().length()>0)  ) {
             session.setAttribute("user", login);
             response.sendRedirect("/user/hello.jsp");
         } else {
